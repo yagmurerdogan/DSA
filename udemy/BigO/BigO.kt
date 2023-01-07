54e6fc260199b92024077a7313759c27b4b8b882
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2023-01-07 23:05:57 +0300

    🦥 Big O notation

---
+++ udemy/BigO/BigO.kt
@@ -0,0 +1,94 @@
+/*
+Question-1
+Create a func which calculates the sum and product of elements of array
+Find the time complexity for created method
+ */
+
+//Time complexity -> O(n) linear
+fun spofArray(arr: Array<Int>) {
+    var sum = 0    // O(1)
+    var product = 1    // O(1)
+    for(i in arr.indices) {    // O(n)
+        sum += arr[i]    // O(1)
+    }
+    for(i in arr.indices) {    // O(n)
+        product *= arr[i]    // O(1)
+    }
+
+    println("sum -> $sum product -> $product")    // O(1)
+}
+
+
+/*
+Question-2
+Create a func which prints to the console the pairs from given array
+Find the time complexity for created method
+ */
+
+//Time complexity -> O(n^2) quadratic
+fun printPairs(arr: Array<Int>) {
+    for (i in arr.indices) {    // O(n)
+        for (j in arr.indices) {    // O(n)
+            println("${arr[i]},${arr[j]}")    // O(1)
+        }
+    }
+}
+
+//Time complexity -> O(n^2) quadratic
+fun printUnorderedPairs(array: Array<Int>) {
+    for (i in array.indices) {
+        for (j in i + 1 until array.size) {
+            println(array[i].toString() + ", " + array[j])
+        }
+    }
+}
+
+//Time complexity -> O(ab)
+fun printUnorderedPairs(arrA: Array<Int>, arrB: Array<Int>) {
+    for (i in arrA.indices) {
+        for (j in arrB.indices) {
+            if (arrA[i] < arrB[j]) {
+                println("${arrA[i]},${arrB[j]}")
+            }
+        }
+    }
+}
+
+//Time complexity -> O(ab) not O(n^2)!!
+fun printUnorderedPairs(arrA: Array<Int>, arrB: Array<Int>) {
+    for (i in arrA.indices) {
+        for (j in arrB.indices) {
+            if (arrA[i] < arrB[j]) {
+                println("${arrA[i]},${arrB[j]}")
+            }
+        }
+    }
+}
+
+//Time complexity -> O(ab) not O(n^2)!!
+fun printUnorderedPairs(arrA: Array<Int>, arrB: Array<Int>) {
+    for (i in arrA.indices) {    // O(a)
+        for (j in arrB.indices) {    // O(b)
+            for (k in 0..1000000) {    // O(1)!!!
+                println("${arrA[i]},${arrB[j]}")    // O(1)
+            }
+        }
+    }
+}
+
+/*
+Question-6
+Create a method which takes an array as a parameter and reverses it
+Find the runtime of the created method?
+ */
+
+//Time complexity -> O(N) not O(N/2)!! Drop constants
+fun reverseIntArray(arr: Array<Int>) {
+    for (i in 0 until arr.size/2) {    // O(N/2)
+        var other = arr.size-i-1    // O(1)
+        var temp = arr[i]    // O(1)
+        arr[i] = arr[other]    // O(1)
+        arr[other] = temp    // O(1)
+    }
+    println(Arrays.toString(arr))    // O(1)
+}
