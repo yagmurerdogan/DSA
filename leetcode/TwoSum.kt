7a9242fc8973227029e5cb09ad2d57b8c05f25b0
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2023-01-10 20:22:27 +0300

    🎈 Two sum

---
+++ leetcode/TwoSum.kt
@@ -0,0 +1,10 @@
+fun twoSum(nums: IntArray, target: Int): IntArray? {
+    for (i in nums.indices) {
+        for (j in i + 1 until nums.size) {
+            if (nums[i] + nums[j] == target) {
+                return intArrayOf(i, j)
+            }
+        }
+    }
+    throw IllegalArgumentException("No solution found")
+}
